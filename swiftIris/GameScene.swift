3ce477b8a23a51929f798aa39736f43c68b0ee46
Nathan Zhang <natz@Nathans-MacBook-Pro-2.local>
2015-10-29 00:38:02 -0700

    swiftIrisGame

---
+++ swiftIris/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  swiftIris
+//
+//  Created by Nathan Zhang on 10/29/15.
+//  Copyright (c) 2015 Nathan Zhang. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+      
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ swiftIris/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  swiftIris
+//
+//  Created by Nathan Zhang on 10/29/15.
+//  Copyright (c) 2015 Nathan Zhang. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
